d513f73f3dbe320cf3c3c275c96b2eca0a2b1b84
LouBege <wolfla@hotmail.fr>
2020-02-06 10:22:36 +0100

    feat : change order detail

--- src/Beans/OrderDetail.java
+++ src/Beans/OrderDetail.java
@@ -1,4 +1,34 @@
 package Beans;
 
 public class OrderDetail {
+    private Order order;
+    private Product product;
+    private int quantity;
+
+    public OrderDetail() {
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
