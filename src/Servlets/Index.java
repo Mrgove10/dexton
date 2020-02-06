27063320b4398d0bfd86329e9bbcd492857e2874
Adrien RICHARD <richardadrien0@gmail.com>
2020-02-06 09:31:38 +0100

    added Classes

--- src/Servlets/Index.java
+++ src/Servlets/Index.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Index")
+@WebServlet(name = "Index", urlPatterns = {"/Home"})
 public class Index extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
