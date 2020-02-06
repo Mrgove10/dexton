36f28664d624730753b3cec0a9b20990db807943
Edouard CLISSON <edouardclisson@gmail.com>
2020-02-06 10:23:20 +0100

    Feat(DAO): Add create user implem

--- src/DAO/DAOConnection.java
+++ src/DAO/DAOConnection.java
@@ -7,7 +7,7 @@ public class DAOConnection {
         Connection connexion = null;
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             System.out.println("test1");
         }
---
+++ src/DAO/UserDAO.java
@@ -0,0 +1,49 @@
+package DAO;
+
+import Beans.User;
+
+import java.sql.*;
+
+public class UserDAO extends DAO<User> {
+    public UserDAO(Connection conn) {
+        super(conn);
+    }
+
+    public boolean create(User obj) {
+        try {
+            PreparedStatement preparedStatement = this.connect.prepareStatement("INSERT INTO USERS(last_name, first_name, email) VALUES (?,?,?);");
+            preparedStatement.setString(1, obj.getLastName());
+            preparedStatement.setString(2, obj.getFirstName());
+            preparedStatement.setString(3, obj.getEmail());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            // Fermeture de la connexion
+            try {
+                if (this.connect != null)
+                    this.connect.close();
+                    return true;
+            } catch (SQLException ignore) {
+                return false;
+            }
+        }
+    }
+
+    public boolean delete(User obj) {
+        return false;
+    }
+
+    public boolean update(User obj) {
+        return false;
+    }
+
+    public User find(int id) {
+        User User = new User();
+
+        return User;
+    }
+}
