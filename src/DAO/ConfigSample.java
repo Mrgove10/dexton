c8e9bedfaf7998b78410a2bb26ef2a1ac407fe28
Adrien RICHARD <richardadrien0@gmail.com>
2020-02-06 09:34:14 +0100

    changed sample

--- src/DAO/ConfigSample.java
+++ src/DAO/ConfigSample.java
@@ -1,4 +1,7 @@
 package DAO;
 
 public class ConfigSample {
+    public final static String path = "jdbc:mysql://localhost:3306/utilisateurs?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
+    public final static String user = "root";
+    public final static String password = "";
 }
