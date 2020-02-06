07aae80dc1feb54d4706d464b7b517655bfe1622
Edouard CLISSON <edouardclisson@gmail.com>
2020-02-06 10:34:39 +0100

    Fix(DAO): del test create user

--- src/Servlets/Index.java
+++ src/Servlets/Index.java
@@ -18,12 +18,12 @@ public class Index extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        User user = new User();
+        /*User user = new User();
         user.setLastName("CLISSON");
         user.setFirstName("Edouard");
         user.setEmail("edouardclisson@gmail.com");
         UserDAO userDAO = new UserDAO(DAOConnection.ConnectDb());
-        userDAO.create(user);
+        userDAO.create(user);*/
         this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
     }
 }
