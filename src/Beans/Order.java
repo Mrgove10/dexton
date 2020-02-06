ce0e2454e468531eb397e39085e1692c5373ef09
Adrien RICHARD <richardadrien0@gmail.com>
2020-02-06 10:21:18 +0100

    [add] order

--- src/Beans/Order.java
+++ src/Beans/Order.java
@@ -1,4 +1,31 @@
 package Beans;
 
 public class Order {
+    private int id;
+    private String status;
+    private User User;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return User;
+    }
+
+    public void setUser(User user) {
+        User = user;
+    }
 }
