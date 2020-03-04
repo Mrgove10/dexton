3afb3c9a667df1ab2d7caa1c3c15536d56f43094
Marc-Antoine <marco38000@gmail.com>
2020-03-04 13:58:34 +0100

    Feat: add inscription

--- src/Servlets/Inscription.java
+++ src/Servlets/Inscription.java
@@ -1,5 +1,9 @@
 package Servlets;
 
+import Beans.User;
+import DAO.Classes.UserDAO;
+import DAO.DAOConnection;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,13 +13,21 @@ import java.io.IOException;
 
 @WebServlet(name = "Inscription")
 public class Inscription extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("ok");
-        response.sendRedirect( request.getContextPath() + "/Login");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        User user = new User();
+        user.setFirstName(request.getParameter("prenom"));
+        user.setLastName(request.getParameter("nom"));
+        user.setEmail(request.getParameter("email"));
+        user.setPassword(request.getParameter("pwd"));
+        try{
+            UserDAO userDAO = new UserDAO(DAOConnection.ConnectDb());
+            userDAO.create(user);
+            response.sendRedirect( request.getContextPath() + "/Login");
+        } catch (Exception e){
+            response.sendRedirect( request.getContextPath() + "/Signin");
+        }
     }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         this.getServletContext().getRequestDispatcher("/WEB-INF/Inscription.jsp").forward(request, response);
     }
 }
