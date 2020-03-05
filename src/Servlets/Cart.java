6070e8db1f24efad6f358f4070ba7a55765f6b9b
Marc-Antoine <marco38000@gmail.com>
2020-03-05 14:07:03 +0100

    Auto stash before merge of "frontend" and "origin/frontend"

--- src/Servlets/Cart.java
+++ src/Servlets/Cart.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "Cart", urlPatterns = {"/Cart"})
@@ -14,7 +15,8 @@ public class Cart extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        HttpSession session = request.getSession();
+        request.setAttribute("cart", session.getAttribute("cart"));
         this.getServletContext().getRequestDispatcher("/WEB-INF/Cart.jsp").forward(request, response);
     }
 }
--- web/WEB-INF/Cart.jsp
+++ web/WEB-INF/Cart.jsp
@@ -3,7 +3,19 @@
 <%@ include file="Header.jsp" %>
 
 <div class="container">
-    <h1>Your Cart</h1>
+    <h1 class="text-center">Your Cart</h1>
+
+    <div class="product-widget">
+        <div class="product-img">
+            <img src="./img/product01.png" alt="">
+        </div>
+        <div class="product-body">
+            <h3 class="product-name"><a href="#">product name goes here</a></h3>
+            <h4 class="product-price"><span class="qty">1x</span>$980.00</h4>
+        </div>
+        <button class="delete"><i class="fa fa-close"></i></button>
+    </div>
+
 </div>
 
 <%@ include file="Footer.jsp" %>
