276da23ab695b5dc0a91832f7cb9dd47b26d6094
LouBege <wolfla@hotmail.fr>
2020-03-05 14:28:53 +0100

    feat : get the name of the current category

--- src/Servlets/Products.java
+++ src/Servlets/Products.java
@@ -1,5 +1,10 @@
 package Servlets;
 
+import Beans.Category;
+import DAO.Classes.CategoryDAO;
+import DAO.Classes.ProductDAO;
+import DAO.DAOConnection;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +15,11 @@ import java.io.IOException;
 @WebServlet(name = "Products", urlPatterns = {"/All", "/Laptops", "/Smartphones", "/Cameras", "/Accessories"})
 public class Products extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String url = request.getRequestURL().toString();
+        var arrayUrl = url.split("/");
+        var categoryName = arrayUrl[arrayUrl.length-1];
+        System.out.println(categoryName);
+        
 
         this.getServletContext().getRequestDispatcher("/WEB-INF/Products.jsp").forward(request, response);
     }
