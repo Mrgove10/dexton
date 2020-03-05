0d8a91426d0878ce202ea80348df153e6d20795b
LouBege <wolfla@hotmail.fr>
2020-03-05 14:07:22 +0100

    feat : add jsp navigation

--- src/Servlets/Products.java
+++ src/Servlets/Products.java
@@ -1,11 +1,5 @@
 package Servlets;
 
-
-import Beans.Role;
-import Beans.User;
-import DAO.DAOConnection;
-import DAO.Classes.UserDAO;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,15 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Products", urlPatterns = {"/All"})
+@WebServlet(name = "Products", urlPatterns = {"/All", "/Laptops", "/Smartphones", "/Cameras", "/Accessories"})
 public class Products extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         this.getServletContext().getRequestDispatcher("/WEB-INF/Products.jsp").forward(request, response);
     }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
 }
 
