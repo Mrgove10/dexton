e1457d530f7db1adae82abed1bad60d852d28a77
Adrien RICHARD <richardadrien0@gmail.com>
2020-03-09 22:06:15 +0100

    added admin draft 1

--- src/Servlets/Admin.java
+++ src/Servlets/Admin.java
@@ -1,11 +1,16 @@
 package Servlets;
 
+import Beans.Product;
+import DAO.Classes.ProductDAO;
+import DAO.DAOConnection;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "Admin", urlPatterns = {"/Admin"})
 public class Admin extends HttpServlet {
@@ -15,6 +20,11 @@ public class Admin extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        ProductDAO productDAO = new ProductDAO(DAOConnection.ConnectDb());
+        var listProducts = new ArrayList<Product>();
+        listProducts = productDAO.find();
+
+        request.setAttribute("listProducts", listProducts);
         this.getServletContext().getRequestDispatcher("/WEB-INF/Admin.jsp").forward(request, response);
     }
 }
--- web/WEB-INF/Admin.jsp
+++ web/WEB-INF/Admin.jsp
@@ -2,7 +2,13 @@
 
 <%@ include file="Header.jsp" %>
 <div class="container">
-
+    <h1>Page d'admininstration</h1>
+    <c:if test="${listProducts.isEmpty() == false}">
+        <ul>
+            <c:forEach items="${listProducts}" var="product">
+                <li> ${product.getName()}</li>
+            </c:forEach>
+        </ul>
+    </c:if>
 </div>
-
 <%@ include file="Footer.jsp" %>
